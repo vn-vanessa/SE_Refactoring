0edac296cf88d6d3684dd26363f0cae316856bcc
vn-vanessa <vanessa.vn@icloud.com>
2021-05-06 17:43:30 +0200

    Update README

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # SE_Refactoring
 
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/ea37fe41f6a14df09b0c059a13227b8e)](https://app.codacy.com/gh/vn-vanessa/SE_Refactoring?utm_source=github.com&utm_medium=referral&utm_content=vn-vanessa/SE_Refactoring&utm_campaign=Badge_Grade_Settings)
+
+### Helpful IntelliJ Features
+The IDE IntelliJ proviedes a lot of helpful features for refactoring:
+
+Feature | How to use
+-------- | -------- 
+Reformat Code   | Strg + Alt + L   
+Refactor Item   | Select Item, press Ctrl + Alt + Shift + T
+Create Test     | Place caret at the class for which you want to create a test, press Alt + Enter -> select Create Test 
+Optimize Imports| Strg + Alt + O
--- src/main/java/fowler/Program.java
+++ src/main/java/fowler/Program.java
@@ -15,11 +15,11 @@ public class Program {
         System.out.println("Welcome to the Movie Store");
         Movie movie1 = new Movie("movie1", PriceCode.NEW_RELEASE);
         Movie movie2 = new Movie("movie2", PriceCode.CHILDRENS);
-        Rental rental = new Rental(movie1, 10);
-        Rental rental1 = new Rental(movie2, 5);
+        Rental rental1 = new Rental(movie1, 10);
+        Rental rental2 = new Rental(movie2, 5);
         Customer customer1 = new Customer("joe");
-        customer1.addRental(rental);
         customer1.addRental(rental1);
+        customer1.addRental(rental2);
         System.out.println("Let's get the Statement");
         result = customer1.statement();
         System.out.println(result);
