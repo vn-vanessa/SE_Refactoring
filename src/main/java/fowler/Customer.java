48740673114d73d2d5e7b7bd3f1574728cd5d34b
vn-vanessa <vanessa.vn@icloud.com>
2021-05-06 15:01:18 +0200

    Reformat code and optimize imports

--- src/main/java/fowler/Customer.java
+++ src/main/java/fowler/Customer.java
@@ -1,20 +1,30 @@
 package fowler;
 
-import java.lang.*;
-import java.util.*;
+import java.util.Enumeration;
+import java.util.Vector;
 
 class Customer {
     private String name;
     private Vector rentals = new Vector();
-    public Customer (String newname){
+
+    public Customer(String newname) {
         name = newname;
-    };
+    }
+
+    ;
+
     public void addRental(Rental arg) {
         rentals.addElement(arg);
-    };
-    public String getName (){
+    }
+
+    ;
+
+    public String getName() {
         return name;
-    };
+    }
+
+    ;
+
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
@@ -28,12 +38,12 @@ class Customer {
             //determine amounts for each line
             thisAmount = amountFor(each);
             // add frequent renter points
-            frequentRenterPoints ++;
+            frequentRenterPoints++;
             // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints ++;
+                frequentRenterPoints++;
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
+            result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
         //add footer lines
