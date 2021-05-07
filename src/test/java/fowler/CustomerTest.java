b21830f7f2635e67768e5385f8cbf79131d03c30
vn-vanessa <vanessa.vn@icloud.com>
2021-05-07 23:10:28 +0200

    Update tests for Customer + Movie

--- src/test/java/fowler/CustomerTest.java
+++ src/test/java/fowler/CustomerTest.java
@@ -34,13 +34,35 @@ class CustomerTest {
     }
 
     @Test
-    void statement() {
-        testCustomer.addRental(new Rental(new NewRelease("Test"), 5));
+    void statementRegular() {
+        testCustomer.addRental(new Rental(new Regular("Test Regular"), 50));
         String expectedResult = "Rental Record for " + testCustomer.getName() + "\n";
         expectedResult += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
-        expectedResult += "\t" + "Test" + "\t" + "\t" + 5 + "\t" + 15.0 + "\n";
-        expectedResult += "Amount owed is " + 15.0 + "\n";
+        expectedResult += "\t" + "Test Regular" + "\t" + "\t" + 50 + "\t" + 74.0 + "\n";
+        expectedResult += "Amount owed is " + 74.0 + "\n";
+        expectedResult += "You earned " + 1 + " frequent renter points";
+        assertEquals(expectedResult, testCustomer.statement());
+    }
+
+    @Test
+    void statementNewRelease() {
+        testCustomer.addRental(new Rental(new NewRelease("Test New Release"), 10));
+        String expectedResult = "Rental Record for " + testCustomer.getName() + "\n";
+        expectedResult += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
+        expectedResult += "\t" + "Test New Release" + "\t" + "\t" + 10 + "\t" + 30.0 + "\n";
+        expectedResult += "Amount owed is " + 30.0 + "\n";
         expectedResult += "You earned " + 2 + " frequent renter points";
+        assertEquals(expectedResult, testCustomer.statement());
+    }
+
+    @Test
+    void statementChildrens() {
+        testCustomer.addRental(new Rental(new Childrens("Test Childrens"), 5));
+        String expectedResult = "Rental Record for " + testCustomer.getName() + "\n";
+        expectedResult += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
+        expectedResult += "\t" + "Test Childrens" + "\t" + "\t" + 5 + "\t" + 4.5 + "\n";
+        expectedResult += "Amount owed is " + 4.5 + "\n";
+        expectedResult += "You earned " + 1 + " frequent renter points";
         assertEquals(expectedResult, testCustomer.statement());
     }
 
--- src/test/java/fowler/MovieTest.java
+++ src/test/java/fowler/MovieTest.java
@@ -8,14 +8,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MovieTest {
 
     private Movie testMovie;
+    private Rental testRental;
 
     @BeforeEach
     void setUp() {
         testMovie = new Regular("Test");
+        testRental = new Rental(testMovie, 10);
     }
 
     @Test
     void getTitle() {
         assertEquals("Test", testMovie.getTitle());
+    }
+
+    @Test
+    void getMoviePrice() {
+        assertEquals(testMovie.getMoviePrice(testRental.getDaysRented()), 14);
     }
 }
